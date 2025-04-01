2beca7716b6ec1451f8ce8b913163c4d0283f570
ABB22denfor <drexman45@gmail.com>
2025-04-01 09:09:59 +0200

    Reduced number of calls, cleaned up main function and added proper handling for -t flag

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 //  Standard libraries:
+#include <iostream>
 #include <vector>
 #include <string>
+#include <unordered_set>
 
 // Header files:
 #include "../include/response_parser.h"
@@ -8,54 +10,60 @@
 
 
 bool isFloat(const std::string& s) {
-    char* end = nullptr;
-    std::strtof(s.c_str(), &end);
-    return end != s.c_str() && *end == '\0';
+  char* end = nullptr;
+  std::strtof(s.c_str(), &end);
+  return end != s.c_str() && *end == '\0';
 }
 
 
 void execute_args(int argc, char* argv[]){
-  std::vector<char*> parsed_args;
+  const std::unordered_set<std::string> validTimePeriods = {"now", "day", "week"};
+  std::string longitude, latitude;
+
+  std::string response;
+  std::string req_time = "day";
+
+
   for(int i = 1; i < argc; ++i){
     std::string flag = argv[i];
 
     if(flag == "-p" || flag == "--position"){
       if((i+2 < argc) && isFloat(argv[i+1]) && isFloat(argv[i+2])){
         // If input is coordinates:
-        parse_response(request_data(argv[i+1], argv[i+2])); 
+        latitude = argv[i+1];
+        longitude = argv[i+2];
       }
       else if(i+1 < argc){
         // If input is city:
-        double longitude, latitude;
         get_coords(argv[i+1], longitude, latitude);
-        parse_response(request_data(std::to_string(latitude), std::to_string(longitude)));
+      }
+      else{
+        std::cerr << "Options for flag omitted. Aborting..." << '\n';
+        return;
       }
     }
-    else if(flag == "-t"){
-      if(i+1 < argc){
-        parsed_args.push_back(argv[i+1]);
+    else if(flag == "-t" || flag == "--time"){
+      if(i+1 < argc && (validTimePeriods.find(argv[i+1]) != validTimePeriods.end())){
+        req_time = argv[i+1];
       }
+      else if(i+1 == argc){
+        std::cerr << "Options for flag omitted. Aborti, req_timeng..." << '\n';
+        return;
+      }
+      else{
+        std::cerr << "Option invalid. See documentation..." << '\n';
+        return;
+      }
+
     }
   }
-  
+
+  response = request_data(latitude, longitude, req_time);
+  parse_response(response); 
 }
 
 int main(int argc, char* argv[]){
-  std::string response_data;
-  std::string city;
-
   execute_args(argc, argv);
-
-  /*
-  
-  double longitude, latitude;
-  if(get_coords(city, longitude, latitude)){
-    std::cout << city << std::endl;
-  }
-  response_data = request_data(std::to_string(latitude), std::to_string(longitude));
-
-  parse_response(response_data);*/
-
   
   return 0;
 }
