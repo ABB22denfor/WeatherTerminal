d5416eafef49e829b0d12c28af1392f1b1cca4a0
ABB22denfor <drexman45@gmail.com>
2025-04-01 14:15:35 +0200

    Header file for general includes

---
+++ include/pch.h
@@ -0,0 +1,13 @@
+#ifndef PCH_H
+#define PCH_H
+
+#include <iostream>
+#include <string>
+#include <format>
+#include <unordered_set>
+#include <nlohmann/json.hpp>
+#include <curl/curl.h>
+#include <curl/easy.h>
+
+#endif // ! PCH_H
+
