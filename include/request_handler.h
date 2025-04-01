31fb19d5e1595d2ebef533c6655ebb0ebf3e9e69
ABB22denfor <drexman45@gmail.com>
2025-04-01 14:17:13 +0200

    Changed from std::string_view to const std::string&

--- include/request_handler.h
+++ include/request_handler.h
@@ -5,6 +5,6 @@
 
 size_t WriteCallback(void* contents, size_t size, size_t nmemb, std::string* output);
 bool get_coords(const std::string city, std::string& lon, std::string& lat);
-std::string request_data(std::string latitude, std::string longitude, std::string_view req_time);
+std::string request_data(std::string latitude, std::string longitude, const std::string& req_time);
 
 #endif // !REQUEST_HANDLER_H
