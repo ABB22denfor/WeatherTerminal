e96038dac8a06d95363d5678bc1c06fc64a72643
ABB22denfor <drexman45@gmail.com>
2025-04-01 09:08:38 +0200

    Removed unnecessary function

--- src/response_parser.cpp
+++ src/response_parser.cpp
@@ -5,26 +5,14 @@
 
 #include "../include/response_parser.h"
 
-
-std::string get_sys_time(const std::string& req_format){
-  time_t now = time(nullptr);
-  tm* local_time = localtime(&now);
-
-  char buffer[80];
-  strftime(buffer, sizeof(buffer), req_format.c_str(), local_time);
-
-  return std::string(buffer);
-}
-
-std::string parse_response(std::string res){
-  std::string curr_hour = get_sys_time("%H");
+std::string_view parse_response(std::string_view res){
   nlohmann::json json_res = nlohmann::json::parse(res);
 
   std::vector<std::string> time_array = json_res["hourly"]["time"].get<std::vector<std::string>>();
   std::vector<int> temp_array = json_res["hourly"]["temperature_2m"].get<std::vector<int>>();
 
   if(time_array.size() != temp_array.size()){
-    std::cout << "ERR: Invalid Fetch";
+    std::cerr << "ERR: Invalid Fetch";
   }
 
   for(size_t i = 0; i < time_array.size(); ++i) {
