7c65946be2303d9df46180a10f9dc24f1d61f166
ABB22denfor <drexman45@gmail.com>
2025-04-27 20:03:54 +0200

    Updated declaration to match function

--- include/cache.h
+++ include/cache.h
@@ -3,7 +3,7 @@
 
 #include "pch.h"
 
-void write_to_cache(const std::string& filename, std::vector<std::string> write_data);
+void write_to_cache(const std::string& filename, std::vector<std::string> write_data, std::vector<std::string> coords);
 nlohmann::json read_from_cache(const std::string& filename);
 void clear_cache(const std::string& filename);
 
