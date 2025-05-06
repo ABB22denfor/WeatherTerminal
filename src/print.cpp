44314582b48a206f68079c41fea9812158997951
ABB22denfor <drexman45@gmail.com>
2025-05-06 08:05:47 +0200

    Added print_help() function

--- src/print.cpp
+++ src/print.cpp
@@ -25,6 +25,29 @@ bool print_favorites(){
   }
 }
 
+void print_help(){
+  std::string help_str = R"(Usage: ./WeatherApp [<options>]
+
+Flags:
+- -p | --position :  
+    Position of which the app will base it's forecast. Permitted options are: "city" or "lon lat".  
+(default: "None")
+- -t | --time :  
+    Time period of which the app will forecast. Permitted options are "now", "day", "week" or "month".  
+(default: "now")
+- -f | --favorite :  
+    Favorite city or location that the app will save in database. No limit to amount of favorite cities.  
+(default: "None")
+- -c | --clear:  
+    Clears the cache of all saved data.  
+(default: "None")
+- -n | --notification:
+    Sets interval for which to send notifications of current data. Minimum of 15 minutes. 
+(default: "None")
+)";
+  std::cout << help_str;
+}
+
 void print_data(const std::string& data_str, int index){
   if(((index + 1) % 2) == 0 && index != 0){
     std::cout << data_str << '\n';
